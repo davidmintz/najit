c4b242949d31d6ec0f27461b3416dee2b5a3a954
david <david@davidmintz.org>
2021-08-09 21:45:05 -0400

    update composer.* etc

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
