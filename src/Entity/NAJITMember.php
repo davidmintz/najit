df26cec43ecb01602fad3407a73d765c52f601c2
david <david@davidmintz.org>
2021-08-09 21:43:09 -0400

    rename User class -> NAJITMember

---
+++ src/Entity/NAJITMember.php
@@ -0,0 +1,46 @@
+<?php
+/** simple class representing a prospective user of NAJIT's Discourse site */
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class NAJITMember
+{
+
+    /**
+     * @Assert\Email(
+     *     message = "'{{ value }}' is not a valid email address"
+     * )
+     * @Assert\NotBlank(
+     *     message = "Email is required"
+     * )
+     */    
+    private $email;
+
+    public function __construct()
+    {
+        
+    }
+
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail() : String
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail(String $email) : NAJITMember
+    {
+        $this->email = $email;
+
+        return $this;
+    }   
+}
