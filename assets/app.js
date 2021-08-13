42e9a7c7458f842e06eb1ce9af80e8a0e0158f54
david <david@davidmintz.org>
2021-08-13 12:49:43 -0400

    work on form validation

--- assets/app.js
+++ assets/app.js
@@ -16,23 +16,27 @@ var btn = $("button[type=submit");
 var btn_text = btn.children("span").text();
 
 $(function() {
-    btn.on("click",function(e){ // return;
+    btn.on("click",function(e){
         e.preventDefault();
         var label = btn.children("span");
-        btn.children("span").text("").addClass("fas fa-spinner fa-spin").attr("disabled",true);
+        label.text("").addClass("fas fa-spinner fa-spin").attr("disabled",true)
+            //.after(`<span id="status"> checking membership...</span>`);
         console.warn("shit was clicked");
         // console.warn( $("#najit_member_form_email").val())
         $.post("/verify",$("form").serialize())
-        .then(r=>{ 
-            console.log(r);
+        .then(response=>{ 
+            console.log(`valid? ${response.valid}`);
+            if (! response.valid) {
+                console.log("show validation errors!");
+                console.warn(typeof response.messages);
+                
+                // $(".form-error")
+            }
             // check if there are validation errors;
             label.removeClass("fas fa-spinner fa-spin").text(btn_text);
+            // $("#status").remove();
             // and maybe restore button if there are validation errors
 
         });
-        // $.post("/invite",{
-        //     email : $("#najit_member_form_email").val(),
-        //     _token : $("najit_member_form_token").val()
-        // }).then(r=>console.log(r));
     });
 });
